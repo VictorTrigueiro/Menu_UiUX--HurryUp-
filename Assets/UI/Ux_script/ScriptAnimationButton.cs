96dc5dc7ec066628611aebb890069b50b747cc4c
Victor trigueiro <victor.trigueiro.patuvestudios@gmail.com>
2026-05-28 20:01:08 -0300

    0.1
    update

--- Assets/UI/Ux_script/ScriptAnimationButton.cs
+++ Assets/UI/Ux_script/ScriptAnimationButton.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class ScriptAnimationButton : MonoBehaviour
 {
+    //publico
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
